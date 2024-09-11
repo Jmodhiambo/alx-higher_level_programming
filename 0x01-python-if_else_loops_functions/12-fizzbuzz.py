dd2ff66b1f131013acb6965618dcd876862915d8
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-09-12 01:43:09 +0300

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,10 +5,10 @@ def fizzbuzz():
             print("FizzBuzz ", end="")
 
         elif (i % 3 == 0):
-            print("Fizz ", end="")
+            print("Fizz", end=" ")
 
         elif (i % 5 == 0):
-            print("Buzz ", end="")
+            print("Buzz", end=" ")
 
         else:
-            print(i, " ", end="")
+            print(i, end=" ")
