e49d2e014cbd489692ce4495f4fbfe339ff852de
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-09-15 05:02:29 +0300

    Task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    lenght = -(len(my_list))
+    i = -1
+    while (i >= lenght):
+        print("{:d}".format(my_list[i]))
+        i = i + (-1)
