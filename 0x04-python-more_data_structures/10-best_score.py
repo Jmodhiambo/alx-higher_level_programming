b9608209b92b42de66675a2fb94fb62ec5ecf97f
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-09-19 05:08:42 +0300

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    a = 0
-    n = "Mart"
+    a = None
+    n = None
     if a_dictionary is None:
         return (None)
     length = len(a_dictionary)
 
     for k, v in a_dictionary.items():
-        if (v > a):
+        if a is None or v > a:
             a = v
             n = k
     return (n)
