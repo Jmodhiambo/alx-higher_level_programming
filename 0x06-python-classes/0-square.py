001c81d7ee22eaad7829c3b7390f6a8d42022de7
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-02 17:45:38 +0300

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-square
+This module defines an empty class Square that defines a square.
+"""
+
+
+class Square:
+    """Empty class that defines a square."""
+    pass
