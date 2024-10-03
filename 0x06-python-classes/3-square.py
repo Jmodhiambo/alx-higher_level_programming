3eb6346b72ae6433f8ffc3dea561904b2b1b5358
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-03 05:15:41 +0300

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 3-square
+This module defines a class Square by:
+    - Private instance attribute: size
+    - Instantiation with optional size and validation
+"""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """
+        Initializes the square.
+        Ensures size is an integer and >= 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square."""
+        return (self.__size * self.__size)
