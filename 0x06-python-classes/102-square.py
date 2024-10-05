f07998ef3c828c6eaede6aa3e347e299b4697f86
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-05 12:08:52 +0300

    Task 9

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,37 +1,13 @@
-#!/usr/bin/python3
-"""
-Module 102-square
-This module defines a class Square by:
-    - Private instance attribute: size
-    - Property getter and setter for size,
-          ensuring it is a number (float or int)
-    - Public methods: area to return the square's area
-    - Comparator methods to allow comparison of
-          Square instances based on area
-"""
-
-
 class Square:
-    """Class that defines a square."""
-
     def __init__(self, size=0):
-        """
-        Initializes the square with size.
-        Ensures size is a number and >= 0.
-        """
         self.size = size
 
     @property
     def size(self):
-        """Getter method to retrieve the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter method to set the size.
-        Ensures size is a number (float or integer) and >= 0.
-        """
         if not isinstance(value, (int, float)):
             raise TypeError("size must be a number")
         if value < 0:
@@ -39,29 +15,35 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns the area of the square."""
         return self.__size ** 2
 
     def __eq__(self, other):
-        """Defines the == comparison based on square area."""
-        return self.area() == other.area()
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return self.area() == other
 
     def __ne__(self, other):
-        """Defines the != comparison based on square area."""
-        return self.area() != other.area()
+        if isinstance(other, Square):
+            return self.area() != other.area()
+        return self.area() != other
 
     def __lt__(self, other):
-        """Defines the < comparison based on square area."""
-        return self.area() < other.area()
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return self.area() < other
 
     def __le__(self, other):
-        """Defines the <= comparison based on square area."""
-        return self.area() <= other.area()
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return self.area() <= other
 
     def __gt__(self, other):
-        """Defines the > comparison based on square area."""
-        return self.area() > other.area()
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return self.area() > other
 
     def __ge__(self, other):
-        """Defines the >= comparison based on square area."""
+        if isinstance(other, Square):
+            return self.area() >= other.area()
         return self.area() >= other
+
