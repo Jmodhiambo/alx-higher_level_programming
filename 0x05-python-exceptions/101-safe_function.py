acf2867ba3ea0c73a028de6ca0b7034b0bd60688
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-06 17:45:48 +0300

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        return (fct(args[0], args[1]))
+        return fct(*args)
     except Exception as err:
         print(f"Exception: {err}", file=sys.stderr)
         return None
