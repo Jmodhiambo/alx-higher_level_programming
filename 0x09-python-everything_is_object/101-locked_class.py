0540fc503219bec41cd01ddd824d897e29392b75
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-09 17:04:38 +0300

    Task 31

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a class `LockedClass` that restricts the creation of
+instance attributes to only `first_name`. Any attempt to create other
+instance attributes dynamically will raise an `AttributeError`.
+"""
+
+
 class LockedClass:
+    """
+    A class that only allows dynamic creation of an instance attribute
+    called 'first_name'. Any attempt to create other attributes
+    will result in an AttributeError.
+    """
     __slots__ = ['first_name']
