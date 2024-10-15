fee6bc187280fd91b71be97eb7af35429918fbd5
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-15 03:09:56 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module has:
+    - Public instance method: def area(self).
+"""
+
+
+class BaseGeometry:
+    """This is an empty class."""
+
+    def area(self):
+        """Raise an error with the message "area() is not implemented"."""
+        raise Exception("area() is not implemented")
