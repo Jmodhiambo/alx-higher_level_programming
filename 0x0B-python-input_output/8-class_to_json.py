e0713e475d3cfa720776804bad4652b6b82284b4
Jmodhiambo <jmodhiambo9346@gmail.com>
2024-10-17 03:09:07 +0300

    Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@ This module provides a function that returns the dictionary description
 with simple data structure for JSON serialization of an object.
 """
 
+
 def class_to_json(obj):
     """
     Returns the dictionary description with simple data structure
