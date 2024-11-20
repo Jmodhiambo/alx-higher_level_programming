f8325670e2d34593006bf400f6794262d29f6a56
jmodhiambo <jmodhiambo9346@gmail.com>
2024-11-20 16:54:59 +0300

    Task 0

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,5 @@
---Lists all databases in MySQL server.
-SHOW DATABASES;
+-- This script Lists all databases in MySQL server.
+-- The SCHEMA_NAME column in INFORMATION_SCHEMA.SCHEMATA contains the names of all databases.
+SELECT SCHEMA_NAME
+FROM INFORMATION_SCHEMA.SCHEMATA 
+ORDER BY SCHEMA_NAME;
