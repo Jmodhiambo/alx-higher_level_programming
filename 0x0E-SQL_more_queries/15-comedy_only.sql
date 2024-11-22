fe1c339930b6ffd40145bf657f59b44e2c6ac6f2
jmodhiambo <jmodhiambo9346@gmail.com>
2024-11-22 17:39:29 +0300

    Task

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,8 @@
+-- Script to list all Comedy shows from the database hbtn_0d_tvshows
+-- Each record displays: tv_shows.title
+-- Results are sorted in ascending order by show title
+-- The tv_genres table contains only one record where name = 'Comedy' (id can be different)
+
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
