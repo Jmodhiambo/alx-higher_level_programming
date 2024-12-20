4bc2af3ca3d6d01991f9ad6fc57d2c9691cf9aae
jmodhiambo <jmodhiambo9346@gmail.com>
2024-12-20 08:05:46 +0300

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,12 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 1 && h > 1) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
     } else {
-      this.width = NaN;
-      this.height = NaN;
+      return {}; /* Returns an empty object. */
     }
   }
 
