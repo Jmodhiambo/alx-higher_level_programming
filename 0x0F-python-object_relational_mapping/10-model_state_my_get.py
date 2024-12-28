2779075fd93b08cf44f031797eb17fd1311f498b
jmodhiambo <jmodhiambo9346@gmail.com>
2024-12-28 13:13:55 +0300

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    # Unpacking arguments
+    username, password, database, state_name = sys.argv[1:5]
+
+    # Create engine and connect to the database
+    eng_url = (
+        'mysql+mysqldb://{}:{}@localhost/{}'
+    ).format(username, password, database)
+
+    engine = create_engine(eng_url, pool_pre_ping=True)
+
+    # Start a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
