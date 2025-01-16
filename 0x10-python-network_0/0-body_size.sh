45ef975c1710ba9d12fbd47cfad0b9ffee9d9a3a
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-16 21:45:40 +0300

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # This script displays the size of the body of an HTTP response using curl
-
-# Ensure the script is called with a URL argument
-if [ -z "$1" ]; then
-    exit 1
-fi
-
-# Send a request to the URL and display the size of the body in bytes
 curl -sI $1 | grep "Content-Length" | cut -d " " -f2
