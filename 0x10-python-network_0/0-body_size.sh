deb9419394a6d061e5ce76df5bbb4123045c29d0
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-16 21:35:50 +0300

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-# This script displays the size of the body of http response using curl command
+# This script displays the size of the body of an HTTP response using curl
 
+# Ensure the script is called with a URL argument
+if [ -z "$1" ]; then
+    exit 1
+fi
+
+# Send a request to the URL and display the size of the body in bytes
 curl -s "$1" | wc -c
