2fd6f4872134de9019e23a57d33ca7c84927b8ba
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-17 04:25:31 +0300

    Technical interview preparations

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,6 @@ def find_peak(list_of_integers):
         return None
 
     lt = list_of_integers
-
     length = len(lt)
 
     # Return the item if it's only in the list
@@ -20,10 +19,21 @@ def find_peak(list_of_integers):
     # Finds the mid value to use as starting point
     mid_val = length // 2
 
-    if lt[mid_val] >= lt[mid_val - 1] and lt[mid_val] >= lt[mid_val + 1]:
-        return lt[mid_val]
-
-    elif lt[mid_val - 1] > lt[mid_val]:
-        return find_peak(lt[:mid_val])
-    else:
-        return find_peak(lt[mid_val + 1:])
+    # Boundary checks for mid_value
+    if mid_val > 0 and mid_val < length - 1:
+        if lt[mid_val] >= lt[mid_val - 1] and lt[mid_val] >= lt[mid_val + 1]:
+            return lt[mid_val]
+        elif lt[mid_val - 1] > lt[mid_val]:
+            return find_peak(lt[:mid_val])  # search in left half
+        else:
+            return find_peak(lt[mid_val + 1:])  # search in right half
+    elif mid_value == 0:  # If the first element is the peak
+        if lt[0] >= lt[1]:
+            return lt[0]
+        else:
+            return find_peak(lt[1:])
+    else:  # If the last element is the peak
+        if lt[-1] >= lt[-2]:
+            return lt[-1]
+        else:
+            return find_peak(lt[:-1])
