3542065ca42bce670be525bab770181fc5c6fd3f
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-17 04:41:03 +0300

    Technical

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,35 @@
 
 def find_peak(list_of_integers):
     """ Returns a peak value from a list"""
+
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
+
+"""
+def find_peak(list_of_integers):
+    "" Returns a peak value from a list""
     if not list_of_integers:
         return None
 
@@ -36,4 +65,4 @@ def find_peak(list_of_integers):
         if lt[-1] >= lt[-2]:
             return lt[-1]
         else:
-            return find_peak(lt[:-1])
+            return find_peak(lt[:-1])"""
