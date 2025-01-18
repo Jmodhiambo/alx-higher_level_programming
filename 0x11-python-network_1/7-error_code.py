28d4d50c77c4bd2609c71e3b656b7f0902f2b3ad
jmodhiambo <jmodhiambo9346@gmail.com>
2025-01-18 05:00:28 +0300

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script sends a request to the URL and displays response's body"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    # Check if the error code 400 and above
+    if int(response.status_code) >= 400:
+        print(f"Error code: {response.status_code}")
