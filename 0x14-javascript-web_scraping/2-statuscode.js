78c8764f4b3eaf8e9e9ba4f070085877260c5a4e
jmodhiambo <jmodhiambo9346@gmail.com>
2025-02-12 05:32:36 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  if (response) {
+  if (error) {
+    console.log(error);
+  } else {
     console.log('code:', response.statusCode);
   }
 });
